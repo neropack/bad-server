6a98c408576809ccd5d228fdc3264e9a418eaf5a
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-11-01 14:39:57 +0300

    fix/ file mw limit

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -1,35 +1,52 @@
-import { Request } from 'express'
-import multer, { FileFilterCallback } from 'multer'
-import { join } from 'path'
+import { Request, Express } from 'express'
+import multer, { diskStorage, FileFilterCallback } from 'multer'
+import { extname, join } from 'path'
+import sharp from 'sharp'
+import fs from 'fs'
+import BadRequestError from '../errors/bad-request-error'
+
+const createDirectoryIfNotExist = (directory: string) => {
+    if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory, { recursive: true })
+    }
+}
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
 
-const storage = multer.diskStorage({
+const storage = diskStorage({
     destination: (
         _req: Request,
         _file: Express.Multer.File,
         cb: DestinationCallback
     ) => {
-        cb(
-            null,
-            join(
-                __dirname,
-                process.env.UPLOAD_PATH_TEMP
-                    ? `../public/${process.env.UPLOAD_PATH_TEMP}`
-                    : '../public'
-            )
+        const uploadDir = join(
+            __dirname,
+            process.env.UPLOAD_PATH_TEMP
+                ? `../public/${process.env.UPLOAD_PATH_TEMP}`
+                : '../public'
         )
-    },
 
+        try {
+            createDirectoryIfNotExist(uploadDir)
+
+            cb(null, uploadDir)
+        } catch (err) {
+            cb(
+                new BadRequestError(`Не удалось создать директорию: ${err}`),
+                uploadDir
+            )
+        }
+    },
     filename: (
         _req: Request,
         file: Express.Multer.File,
         cb: FileNameCallback
     ) => {
-        //cb(null, file.originalname)
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, `${uniqueSuffix}-${file.originalname}`)
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`
+        const newFilename = uniqueSuffix + extname(file.originalname)
+
+        cb(null, newFilename)
     },
 })
 
@@ -41,17 +58,57 @@ const types = [
     'image/svg+xml',
 ]
 
+const fileSize = {
+    min: 2 * 1024,
+    max: 10 * 1024 * 1024,
+}
+
+const imgMinRes = {
+    width: 50,
+    height: 50,
+}
+
 const fileFilter = (
     _req: Request,
     file: Express.Multer.File,
     cb: FileFilterCallback
 ) => {
     if (!types.includes(file.mimetype)) {
-        return cb(null, false)
+        // return cb(null, false)
+        return cb(
+            new Error(`Недопустимый тип файла. Допустимые: ${types.join(', ')}`)
+        )
     }
 
-    return cb(null, true)
+    // return cb(null, true)
+    if (file.size < fileSize.min) {
+        return cb(
+            new Error(
+                `Размер файла слишком маленький. Минимальный размер - 2 KB`
+            )
+        )
+    }
+
+    sharp(file.buffer)
+        .metadata()
+        .then((metadata) => {
+            if (
+                metadata.width != null &&
+                metadata.height != null &&
+                (metadata.width < imgMinRes.width ||
+                    metadata.height < imgMinRes.height)
+            ) {
+                return cb(
+                    new Error(
+                        `Минимальное разрешение изображения: ${imgMinRes.width}x${imgMinRes.height}px`
+                    )
+                )
+            }
+
+            return cb(null, true)
+        })
+        .catch(() => cb(new Error('Ошибка при обработке изображения')))
 }
 
-const limits = { fileSize: 5 * 1024 * 1024 } // 5MB
+const limits = { fileSize: fileSize.max } // 10MB
 export default multer({ storage, fileFilter, limits })
